0e908bc8fccaff75bb35175eb9e7cfb02bee5a26
Br34kInFr33 <62271814+Br34kInFr33@users.noreply.github.com>
2020-03-17 03:49:35 -0500

    Update autoup.php

--- autoup.php
+++ autoup.php
@@ -11,7 +11,7 @@ define('JOB_LOG', ROOT_PATH.'/jobs');
 
 define('SITE_ROOT', 'http://test.site');
 define('ANNOUNCE_URL', 'http://test.site/announce.php');
-define('QUICK_LOGIN', 'http://51.83.72.245/pagelogin.php?qlogin=90e763c53c9da922384a5aef982f7c5b533f0770efae259161c87d0a889bcf3d320bc88839ba8a8e93a29dcb19896631');8
+define('QUICK_LOGIN', 'http://test.site/pagelogin.php?qlogin=90e763c53c9da922384a5aef982f7c5b533f0770efae259161c87d0a889bcf3d320bc88839ba8a8e93a29dcb19896631');8
 
 define('TMDB_API', '2a2c0a99175ae7746578c600d8c744f7');
 
